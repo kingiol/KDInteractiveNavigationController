ae9fa392dbf5fb90fadb9f6568bf8ac5067d9b5c
kingiol <kingxiaokang@gmail.com>
2015-11-21 14:18:18 +0800

    init

---
+++ KDInteractiveNavigationController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KDInteractiveNavigationController
+//
+//  Created by Kingiol on 15/11/21.
+//  Copyright © 2015年 Kingiol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
