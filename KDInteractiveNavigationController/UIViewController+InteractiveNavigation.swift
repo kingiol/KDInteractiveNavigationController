e930d26f6fe6d987a92c30e6183daaeefbe8e47c
kingiol <kingxiaokang@gmail.com>
2017-10-16 15:00:29 +0800

    update

--- KDInteractiveNavigationController/UIViewController+InteractiveNavigation.swift
+++ KDInteractiveNavigationController/UIViewController+InteractiveNavigation.swift
@@ -16,26 +16,27 @@ protocol SelfAware: class {
     static func awake()
 }
 
-class NothingToSeeHere {
-    static func harmlessFunction() {
-        let typeCount = Int(objc_getClassList(nil, 0))
-        let types = UnsafeMutablePointer<AnyClass>.allocate(capacity: typeCount)
-        let autoreleasingTypes = AutoreleasingUnsafeMutablePointer<AnyClass>(types)
-        objc_getClassList(autoreleasingTypes, Int32(typeCount))
-        for index in 0 ..< typeCount { (types[index] as? SelfAware.Type)?.awake() }
-        types.deallocate(capacity: typeCount)
-    }
-}
-extension UIApplication {
-    private static let runOnce: Void = {
-        NothingToSeeHere.harmlessFunction()
-    }()
-    override open var next: UIResponder? {
-        // Called before applicationDidFinishLaunching
-        UIApplication.runOnce
-        return super.next
-    }
-}
+//class NothingToSeeHere {
+//    static func harmlessFunction() {
+//        let typeCount = Int(objc_getClassList(nil, 0))
+//        let types = UnsafeMutablePointer<AnyClass>.allocate(capacity: typeCount)
+//        let autoreleasingTypes = AutoreleasingUnsafeMutablePointer<AnyClass>(types)
+//        objc_getClassList(autoreleasingTypes, Int32(typeCount))
+//        for index in 0 ..< typeCount { (types[index] as? SelfAware.Type)?.awake() }
+//        types.deallocate(capacity: typeCount)
+//    }
+//}
+//extension UIApplication {
+//    private static let runOnce: Void = {
+//        NothingToSeeHere.harmlessFunction()
+//    }()
+//    override open var next: UIResponder? {
+//        // Called before applicationDidFinishLaunching
+//        UIApplication.runOnce
+//        return super.next
+//    }
+//}
+
 @IBDesignable
 extension UIViewController: SelfAware {
     static func awake() {
