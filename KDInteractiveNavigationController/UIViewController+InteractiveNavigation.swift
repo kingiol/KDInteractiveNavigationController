89398e22728f743ce37d9d0649b3e64f29f6b989
kingiol <kingxiaokang@gmail.com>
2017-10-23 12:02:26 +0800

    update

--- KDInteractiveNavigationController/UIViewController+InteractiveNavigation.swift
+++ KDInteractiveNavigationController/UIViewController+InteractiveNavigation.swift
@@ -11,47 +11,47 @@ import UIKit
 private var interactiveNavigationBarHiddenAssociationKey: UInt8 = 0
 
 extension UIApplication {
-  override open var next: UIResponder? {
-    UIViewController.awake
-    return super.next
-  }
+    override open var next: UIResponder? {
+        UIViewController.awake
+        return super.next
+    }
 }
 
 extension UIViewController {
-  static let awake : Void  = {
-    replaceInteractiveMethods()
-    return
-  }()
+    static let awake : Void  = {
+        replaceInteractiveMethods()
+        return
+    }()
   
-  @IBInspectable public var interactiveNavigationBarHidden: Bool {
-    get {
-      var associateValue = objc_getAssociatedObject(self, &interactiveNavigationBarHiddenAssociationKey)
-      if associateValue == nil {
-        associateValue = false
-      }
-      return associateValue as! Bool;
+    @IBInspectable public var interactiveNavigationBarHidden: Bool {
+        get {
+            var associateValue = objc_getAssociatedObject(self, &interactiveNavigationBarHiddenAssociationKey)
+            if associateValue == nil {
+                associateValue = false
+            }
+            return associateValue as! Bool;
+        }
+        set {
+            objc_setAssociatedObject(self, &interactiveNavigationBarHiddenAssociationKey, newValue, .OBJC_ASSOCIATION_RETAIN)
+        }
     }
-    set {
-      objc_setAssociatedObject(self, &interactiveNavigationBarHiddenAssociationKey, newValue, .OBJC_ASSOCIATION_RETAIN)
+  
+    fileprivate static func replaceInteractiveMethods() {
+        method_exchangeImplementations(
+            class_getInstanceMethod(self, #selector(UIViewController.viewWillAppear(_:)))!,
+            class_getInstanceMethod(self, #selector(UIViewController.KD_interactiveViewWillAppear(_:)))!)
     }
-  }
   
-  fileprivate static func replaceInteractiveMethods() {
-    method_exchangeImplementations(
-      class_getInstanceMethod(self, #selector(UIViewController.viewWillAppear(_:)))!,
-      class_getInstanceMethod(self, #selector(UIViewController.KD_interactiveViewWillAppear(_:)))!)
-  }
-  
-  @objc func KD_interactiveViewWillAppear(_ animated: Bool) {
-    KD_interactiveViewWillAppear(animated)
-    let excludeVCs = [
-      "CKSMSComposeRemoteViewController",
-      "CKSMSComposeController",
-      ]
-    let vcName = NSStringFromClass(type(of: self))
-    if excludeVCs.contains(vcName) { return }
-    navigationController?.setNavigationBarHidden(interactiveNavigationBarHidden, animated: animated)
-  }
+    @objc func KD_interactiveViewWillAppear(_ animated: Bool) {
+        KD_interactiveViewWillAppear(animated)
+        let excludeVCs = [
+            "CKSMSComposeRemoteViewController",
+            "CKSMSComposeController",
+        ]
+        let vcName = NSStringFromClass(type(of: self))
+        if excludeVCs.contains(vcName) { return }
+        navigationController?.setNavigationBarHidden(interactiveNavigationBarHidden, animated: animated)
+    }
   
 }
 
